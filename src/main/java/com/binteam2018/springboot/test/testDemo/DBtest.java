d95aa9124ca6cfa4e2d24bc22af3788fe219b1b2
binbinTeam <39162513+binbinTeam@users.noreply.github.com>
2018-06-06 18:55:52 +0800

    Initial commit

---
+++ src/main/java/com/binteam2018/springboot/test/testDemo/DBtest.java
@@ -0,0 +1,11 @@
+package com.binteam2018.springboot.test.testDemo;
+
+import org.junit.jupiter.api.Test;
+
+public class DBtest {
+
+    @Test
+    public void Test0(){
+        System.err.println("qweqweqweqweqeqwe");
+    }
+}
